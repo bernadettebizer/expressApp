06970aad9ee3dea3d492c97a5c7781e5d4faa744
Bernadette Bizer <bbizer@ITA-5515.local>
2020-06-17 16:16:08 -0400

    Add parsing of post data to prepare for request signing

--- expressApp.js
+++ expressApp.js
@@ -1,11 +1,39 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser');
+const oauth_sign = require('oauth-sign');
+const qs = require('querystring');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
 
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => res.render('pages/index', { title: 'Hey', message: 'Hello, beautiful world!' }))
+//app.get('/', (req, res) => res.render('pages/index', { title: 'Hey', message: 'Hello, beautiful world!' }));
 
-app.get('/about', (req, res) => res.render('pages/about', { title: 'About', message: 'How ya doin today?' }))
+app.get('/about', (req, res) => res.render('pages/about', { title: 'About', message: "my_javascript_variable" }));
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.all('/', (req,res) => {
+	var oauth_consumer_key = req.body.oauth_consumer_key;
+	var oauth_nonce = req.body.oauth_nonce;
+	var oauth_signature_method = req.body.oauth_signature_method;
+	var oauth_timestamp = req.body.oauth_timestamp;
+	var oauth_version = req.body.oauth_version;
+	var oauth_callback = req.body.oauth_callback;
+	
+	if (req.body.lis_person_name_given && req.body.lis_person_name_family) {
+		var full_name = req.body.lis_person_name_given + ' ' + req.body.lis_person_name_family;
+	} else {
+		var full_name = 'friend';
+	}
+	var message = 'hello, ' + full_name + '!';
+	res.render('pages/index', { title: 'Hey', message: message })
+});
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+
+//todo
+//remove node_modules file
+//.ds_store files in a bunch of directories, get rid of those as well
+//use git ignore
