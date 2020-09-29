8dc7ffa8d1e39bd401ed8d69a49e8b66044b6056
Bernadette Bizer <bbizer@ITA-5515.local>
2020-09-29 12:41:54 -0400

    Add logic to wait for ajax response before openning content in html

--- expressApp.js
+++ expressApp.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
-const oauth = require('./scripts/oauth');
+const oauth = require('./lib/oauth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
